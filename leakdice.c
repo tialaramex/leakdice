44a798196f00faa8e9b43a031fd39d459fecc7bd
Nick Lamb <git@tree.tlrmx.org>
2009-01-22 20:03:46 +0000

    use standard GNU proclamation at top of file

--- leakdice.c
+++ leakdice.c
@@ -1,3 +1,26 @@
+/*  leakdice - Monte Carlo sampling of heap data
+
+    Copyright (C) 2009 Nick Lamb
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* This is useful when for some reason a methodical approach to identifying memory leaks isn't available
+ * e.g. because the process is already running and it's too late to instrument it
+ * it's inspired in part by Raymond Chen's blog article "The poor man's way of identifying memory leaks" */
+
 #define _FILE_OFFSET_BITS 64
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -10,14 +33,6 @@
 #include <fcntl.h>
 
 #include <sys/ptrace.h>
-
-/* Nick Lamb, under the GNU GPL */
-
-/* Monte Carlo sampling of heap data */
-
-/* This is useful when for some reason a methodical approach to identifying memory leaks isn't available
- * e.g. because the process is already running and it's too late to instrument it
- * it's inspired in part by Raymond Chen's blog article "The poor man's way of identifying memory leaks" */
 
 static void dump_ascii(off_t offset, uint8_t *buffer, int count)
 {
