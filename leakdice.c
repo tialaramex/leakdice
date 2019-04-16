357897f1701119154ade5f1d420af802d80654bf
Nick Lamb <nlamb@kynd.io>
2019-04-16 10:48:55 +0100

    fix header #includes

--- leakdice.c
+++ leakdice.c
@@ -28,8 +28,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 
 #include <sys/ptrace.h>
